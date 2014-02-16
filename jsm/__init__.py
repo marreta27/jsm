8e894c2d1637dadbe6bea11ea876c98fbe078504
nekanat <nekanat.stock@gmail.com>
2014-02-16 22:28:32 +0900

    bug fix

--- jsm/__init__.py
+++ jsm/__init__.py
@@ -56,7 +56,6 @@ class Quotes(object):
             end_date = datetime.date.today()
         if not start_date:
             start_date = datetime.date.fromtimestamp(time.mktime(end_date.timetuple()) - 2592000)
-        end_date = datetime.date.today()
         return p.get_range(ccode, start_date, end_date)
     
     def get_finance(self, ccode):
